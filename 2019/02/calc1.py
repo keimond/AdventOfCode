8a29ef6e7e717c4647404936086ffda6ca0bd1fa
St.Keimond <cybermacwy@gmail.com>
2019-12-14 20:57:24 -0600

    fixed

--- 2019/02/calc1.py
+++ 2019/02/calc1.py
@@ -5,25 +5,25 @@ with open('input', 'r') as input:
 
 def add(in1, in2, out1):
     global data
-    data[out1] = data[in1] + data[in2]
+    data[out1] = in1 + in2
 
 def multi(in1, in2, out1):
     global data
-    data[out1] = data[in1] * data[in2]
+    data[out1] = in1 * in2
 pos = 0
 while True:
     if data[pos] == 1:
-        print("Position {0} is a {1}".format(pos,data[pos]))
-        print("{0} + {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
+        #print("Position {0} is a {1}".format(pos,data[pos]))
+        #print("{0} + {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
         add(data[pos + 1], data[pos + 2], data[pos + 3])
         pos = pos + 4
     elif data[pos] == 2:
-        print("Position {0} is a {1}".format(pos,data[pos]))
-        print("{0} * {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
+        #print("Position {0} is a {1}".format(pos,data[pos]))
+        #print("{0} * {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
         multi(data[pos + 1], data[pos + 2], data[pos + 3])
         pos = pos + 4
     elif data[pos] == 99:
-        print("Position {0} is a {1}".format(pos,data[pos]))
+        #print("Position {0} is a {1}".format(pos,data[pos]))
         break
 
 print(data)
