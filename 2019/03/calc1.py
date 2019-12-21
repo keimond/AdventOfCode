2d1feed6bf6f6c4c362432a7c1351f375845c431
St.Keimond <cybermacwy@gmail.com>
2019-12-21 15:32:36 -0600

    Working!

--- 2019/03/calc1.py
+++ 2019/03/calc1.py
@@ -1,41 +1,70 @@
+from scipy.spatial import distance
 import json
 import re
 
-routes,points = [],[]
+instructions,points = [],[]
 
 ## Set up two lists of instructions
 with open('input', 'r') as file_input:
   for row in file_input:
     try:
-      routes.append(row.rstrip('\n').split(','))
+      instructions.append(row.rstrip('\n').split(','))
     except:
       break
 
+def addPoint(i,x,y):
+    global points
+    #points[i].append(x)
+    #points[i].append(y)
+    try:
+        if points[i][x]:
+            if y not in points[i][x]:
+                points[i][x].append(y)
+    except:
+        points[i][x] = [y]
+
 i = 0
-for route in routes:
+for route in instructions:
     x, y = 0, 0
-    points.append([])
+    points.append({})
     for instruction in route:
         # Extract direction and numbers from our value
         instruction = re.split('([A-Z])([0-9]+)', instruction)
         direction = instruction[1]
-        distance = int(instruction[2])
+        dist = int(instruction[2])
         if direction == "U":
-            y = y + distance + 1
-            points[i].append(x)
-            points[i].append(y)
+            for d in range(dist):
+                y = y + 1
+                addPoint(i,x,y)
         if direction == "D":
-            y = y - distance + 1
-            points[i].append(x)
-            points[i].append(y)
+            for d in range(dist):
+                y = y - 1
+                addPoint(i,x,y)
         if direction == "R":
-            x = x + distance + 1
-            points[i].append(x)
-            points[i].append(y)
+            for d in range(dist):
+                x = x + 1
+                addPoint(i,x,y)
         if direction == "L":
-            x = x - distance + 1
-            points[i].append(x)
-            points[i].append(y)
+            for d in range(dist):
+                x = x - 1
+                addPoint(i,x,y)
     i =+ 1
 
-print(points[0])
+intersections=[]
+for x in points[0]:
+    for y in points[0][x]:
+        try:
+            if y in points[1][x]:
+                intersections.append(x)
+                intersections.append(y)
+        except:
+            pass
+
+md = []
+p = 0
+for i in range(int(len(intersections)/2)):
+    md.append(distance.cityblock([0,0,0], [intersections[p],intersections[p+1],0]))
+    p += 2
+
+md.sort()
+print(md[0])
--- 2019/05/calc1.py
+++ 2019/05/calc1.py
@@ -1,30 +1,73 @@
 import ast
 
-with open('input', 'r') as input:
-    data = [int(i) for i in input.read().split(",")]
-
-def add(in1, in2, out1):
-    global data
-    data[out1] = data[in1] + data[in2]
-
-def multi(in1, in2, out1):
-    global data
-    data[out1] = data[in1] * data[in2]
-
+with open('input', 'r') as code:
+    data = [int(i) for i in code.read().split(",")]
 pos = 0
+#test = '1002,4,3,4,33'
+#data = test.split(",")
+
+def add(pos1, pos2, pos3):
+    global data
+    data[pos3] = data[pos1] + data[pos2]
+
+def multi(pos1, post2, post3):
+    global data
+    data[pos3] = data[pos1] * data[pos2]
+
+def program(opcode, modes, parameters):
+    global data,pos
+    p = []
+    print("op: {}, modes: {}, parameters: {}".format(opcode,modes,parameters))
+    for i,m in enumerate(modes[:2]):
+        if m == 0:
+            p.append(int(data[parameters[i]]))
+        else:
+            p.append(int(parameters[i]))
+    print(p)
+    if opcode == 1:
+        print('m=1')
+        print('{} = {} + {}'.format(data[parameters[2]],p[0],p[1]))
+        data[parameters[2]] = p[0] + p[1]
+    if opcode == 2:
+        print('m=2')
+        print('{} = {} * {}'.format(data[parameters[2]],p[0],p[1]))
+        data[parameters[2]] = p[0] * p[1]
+
 while True:
+    print('Position = {}'.format(pos))
     if data[pos] == 1:
-        print("Position {0} is a {1}".format(pos,data[pos]))
-        print("{0} + {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
+        print('m=1')
         add(data[pos + 1], data[pos + 2], data[pos + 3])
         pos = pos + 4
     elif data[pos] == 2:
-        print("Position {0} is a {1}".format(pos,data[pos]))
-        print("{0} * {1} to pos {2}".format(data[pos + 1],data[pos + 2],data[pos + 3]))
+        print('m=2')
         multi(data[pos + 1], data[pos + 2], data[pos + 3])
         pos = pos + 4
+    elif data[pos] == 3:
+        print('m=3')
+        IN = int(input("Enter the input value: "))
+        i = data[pos + 1]
+        data[i] = IN
+        pos = pos + 2
+    elif data[pos] == 4:
+        print('m=4')
+        i = data[pos +1]
+        print(data[i])
+        pos = pos + 2
+    elif 4 <= len(str(data[pos])) <= 5:
+        m = list(str(data[pos]))
+        m.reverse()
+        opcode = int(str(m[1]) + str(m[0]))
+        m4 = 0
+        if len(m) == 5:
+            m4 = m[4]
+        program(int(opcode), [int(m[2]),int(m[3]),int(m4)], [int(data[pos + 1]),int(data[pos + 2]),int(data[pos + 3])])
+        pos = pos + 4
     elif data[pos] == 99:
-        print("Position {0} is a {1}".format(pos,data[pos]))
+        print("We were told to terminate at position {}".format(pos))
         break
+    else:
+        print("error.. can't handle {0}".format(data[pos]))
+        quit(1)
 
 print(data)
